aa27178819efefdaf241a96c3e95218b24a38028
dr4inwyaw <p1rkus1337@gmail.com>
2023-11-23 18:37:43 +0300

    friends

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import Navbar from './components/navbar/Navbar.jsx';
 import Contant from './components/contant/Contant.jsx'; 
 import Dialog from './components/dialogs/Dialog.jsx';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-function App({data, data2, data3}) {
+function App({state}) {
   return (
     <div className="wrap">
       <BrowserRouter>
       <Header/>
       <Navbar/>
       <Switch> 
-        <Route exact path='/' render={()=> <Contant data={data} />}/>
-        <Route exact path='/contant' render={()=> <Contant data={data} />}/>
-        <Route exact path='/dialogs' render={()=> <Dialog data2={data2} data3={data3}/>}/>
+        <Route exact path='/' render={()=> <Contant data={state.post.postsName} />}/>
+        <Route exact path='/contant' render={()=> <Contant data={state.post.postsName} />}/>
+        <Route exact path='/dialogs' render={()=> <Dialog data2={state.dialog.dialogName} data3={state.dialog.dialogMes}/>}/>
       </Switch>
       </BrowserRouter>
     </div>
   );
 }
-
+// ошибка только тут
 export default App;
---
+++ src/data/state.jsx
@@ -0,0 +1,25 @@
+import avatar1 from './../img/7300000.jpg'
+
+let state = { 
+    dialog: {
+        dialogName: [{name:"Илон Маск", id: "0" },
+        {name:"Иванов Иван", id: "1" },
+        {name: "Максимов Максим", id: "2" },],
+        dialogMes: [{text:"как дела?"},
+        {text:"чем занимаешься?"},
+        {text:"как настроение?"} ]
+    },
+    post: {
+        postsName: [{ name:"Илон Маск", text:"X-twitter",  img: avatar1, like:'1'},
+        { name:"Иванов Иван", text:"Оцените мою новость",  img: avatar1, like:'2' },
+        { name:"Максимов Максим", text:"купил новую машину", img: avatar1, like:'3'},]
+    },
+    
+    navbar: {
+        friends: [{ name:"Илон Маск", img: avatar1 },
+        { name:"Иванов Иван", img: avatar1 }, 
+        { name:"Максимов Максим", img: avatar1}]
+    }
+}
+
+export default state;
